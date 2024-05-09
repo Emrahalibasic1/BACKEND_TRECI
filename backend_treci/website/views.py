7bfd9ef4cd902e5d1fe3b7f6e79c6dd67afa803c
Emrahalibasic1 <alibasicemrah98@.com>
2024-05-09 07:53:01 +0200

    finish

--- backend_treci/website/views.py
+++ backend_treci/website/views.py
@@ -1,20 +1,37 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, jsonify
 from flask_login import login_required, current_user
-from flask import request,flash 
-from .models import Note
-from . import db
+from flask import request, flash
+from.models import Note
+from. import db
+import json
+
 views = Blueprint('views', __name__)
 
-@views.route('/',methods =['GET','POST'])
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
     if request.method == 'POST':
         note = request.form.get('note')
         if len(note) < 1:
-             flash('Note ne smije biti prazan',category='error')
+            flash('Note ne smije biti prazan', category='error')
         else:
             flash('Uspjesno ste dodali note')
-            new_note=Note(data=note,user_id=current_user.id)
+            new_note = Note(data=note, user_id=current_user.id)
             db.session.add(new_note)
             db.session.commit()
-    return render_template("home.html", user=current_user) 
+    return render_template("home.html", user=current_user)
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    note_id = note['noteId']
+    note = Note.query.get(note_id)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+            return jsonify({})
+    return jsonify({})
+
+
+    
